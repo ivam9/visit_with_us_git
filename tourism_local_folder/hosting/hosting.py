6ca2b0f0ce519d31e85324d822ac735e6d09a40d
ivam9 <mayu.pict@gmail.com>
2026-04-02 07:14:01 +0000

    latest commit

---
+++ tourism_local_folder/hosting/hosting.py
@@ -0,0 +1,42 @@
+# =========================
+# IMPORTS
+# =========================
+from huggingface_hub import HfApi, create_repo
+import os
+import time
+
+# =========================
+# CONFIG
+# =========================
+repo_id = "mayurdotcreate/tourism"
+repo_type = "space"
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+# =========================
+# CREATE SPACE IF NOT EXISTS
+# =========================
+create_repo(
+    repo_id=repo_id,
+    repo_type=repo_type,
+    exist_ok=True
+)
+
+print("✅ Space ready.")
+
+# =========================
+# UPLOAD DEPLOYMENT FILES
+# =========================
+for i in range(3):
+    try:
+        api.upload_folder(
+            folder_path="tourism_local_folder/deployment",
+            repo_id=repo_id,
+            repo_type=repo_type,
+            path_in_repo=""
+        )
+        print("🚀 Deployment successful!")
+        break
+    except Exception as e:
+        print(f"Retry {i+1} failed:", e)
+        time.sleep(5)
